e8cd80ea795e5ecd9837525d0a7a577cbd3d5442
Vit Pavlik <pavlikvitek13@gmail.com>
2024-06-26 20:34:00 +0200

    prevent endless iteration

--- lol-picker.js
+++ lol-picker.js
@@ -1,3 +1,8 @@
+/* safety counter: if there are two people who only want to play bot, for
+example, the while loop loops indefinitely, because it is undecidable. set
+a maximum number of iterations */
+const MAX_ITERATIONS = 250
+
 const NUMBER_OF_PLAYERS = 5
 
 const NUMBER_OF_ROLES = 5
@@ -9,8 +14,25 @@ const ROLES = [
     "top",
 ]
 
+UNDECIDED_ROLE = "🤷‍♀️"
+
+/* get a random item from an array */
 function get_random(list) {
+    if (list.length === 0) {
+        return UNDECIDED_ROLE
+    }
     return list[Math.floor((Math.random()*list.length))];
+}
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+/* https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array */
+function shuffle_array(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
 
 function fill_table() {
@@ -44,7 +66,6 @@ function fill_table() {
 }
 
 function generate_button() {
-    console.log("generate button was pressed")
     const roles_elem = document.getElementById("generated-roles")
     roles_elem.innerHTML = ""
 
@@ -59,6 +80,8 @@ function generate_button() {
     }
 
     /* generate a dictionary of roles for each player */
+    /* also generate a list of players */
+    const player_names = []
     const player_roles = {}
     players.forEach(player => {
 
@@ -68,6 +91,7 @@ function generate_button() {
 
 
         /* add all the roles checked for this player */
+        player_names.push(player_name)
         player_roles[player_name] = []
         ROLES.forEach(role => {
             role_checkbox = document.getElementById(`player${player}-${role}`)
@@ -78,16 +102,19 @@ function generate_button() {
 
     })
 
+    shuffle_array(player_names)
+
     /**
      * in this loop, dictionary mutates
      * from {player: [role1, role2, ...], ...}
      * to {player: role, ...}
     */
     const picked_roles = [];
-    for (let player_name in player_roles) {
+    player_names.forEach(player_name => {
 
         /* pick a random role for player, if role occupied, repeat */
-        let done = false;
+        let done = false
+        let safety_counter = 0
         while (!done) {
             const role = get_random(player_roles[player_name])
             if (!picked_roles.includes(role)) {
@@ -95,8 +122,16 @@ function generate_button() {
                 player_roles[player_name] = role
                 done = true
             }
+
+            /* stop iteration if the proposal is (probably) undecidable */
+            safety_counter++
+            if (safety_counter == MAX_ITERATIONS) {
+                player_roles[player_name] = UNDECIDED_ROLE
+                done = true
+            }
+
         }
-    }
+    });
 
     for (let player_name in player_roles) {
         const text = `${player_name}: ${player_roles[player_name]}`
@@ -104,8 +139,6 @@ function generate_button() {
         text_elem.textContent = text
         roles_elem.appendChild(text_elem)
     }
-    console.log("players: ", players)
-    console.log("roles: ", player_roles)
 
 }
 
